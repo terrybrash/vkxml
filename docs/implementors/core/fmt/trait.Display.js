46e8b4210ad4f7c6502e6fd3b76ade0f4fbf594d
Terry Brashaw <terrybrashaw@gmail.com>
2017-09-22 10:30:45 -0400

    Update docs

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["quote"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"quote/struct.Tokens.html\" title=\"struct quote::Tokens\">Tokens</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"quote/struct.Ident.html\" title=\"struct quote::Ident\">Ident</a>",];
+implementors["serde"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"serde/de/value/struct.Error.html\" title=\"struct serde::de::value::Error\">Error</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"serde/de/enum.Unexpected.html\" title=\"enum serde::de::Unexpected\">Unexpected</a>&lt;'a&gt;","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"trait\" href=\"serde/de/trait.Expected.html\" title=\"trait serde::de::Expected\">Expected</a> + 'a",];
+implementors["syn"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"syn/struct.Ident.html\" title=\"struct syn::Ident\">Ident</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"syn/enum.IntTy.html\" title=\"enum syn::IntTy\">IntTy</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"syn/enum.FloatTy.html\" title=\"enum syn::FloatTy\">FloatTy</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["serde_derive_internals"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"serde_derive_internals/struct.Ctxt.html\" title=\"struct serde_derive_internals::Ctxt\">Ctxt</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
